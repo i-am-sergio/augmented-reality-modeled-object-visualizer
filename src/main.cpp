68e714aad327112c8dbea3ddd33bab6ad54c9cb2
i-am-sergio <sergio15.dev@gmail.com>
2024-07-07 09:03:11 -0500

    source: update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,34 @@
 #include "Load3DModel.hpp"
 #include "ObjectProjection.hpp"
 
-int main(){
-    
-    Load3DModel obj("models/wolf.obj", true, 0, 0, 0.15f, 0.05, 0.0002f);
-    obj.printVertices();
+using namespace std;
 
-    LoadCamera camera;
+void loadAndAddModel(string modelPath, float scaleFactor, vector<ObjectProjection>& objects) {
+    Load3DModel model(modelPath, true, 0, 0, 0.15f, 0.05, scaleFactor);
+    vector<cv::Point3f> vertices = model.getVertices();
+    vector<cv::Point3f> normals = model.getNormals();
+    vector<cv::Point2f> texCoords = model.getTexCoords();
+    vector<Face> faces = model.getFaces();
+    ObjectProjection object(vertices, normals, texCoords, faces);
+    objects.push_back(object);
+}
+
+int main(){
+    // Load 3D models
+    vector<ObjectProjection> objects;
+    loadAndAddModel("models/wolf.obj", 0.0004f, objects);
+    loadAndAddModel("models/rat.obj", 0.004f, objects);
+    loadAndAddModel("models/fox.obj", 0.002f, objects);
+
+    // Load camera
+    LoadCamera camera(objects);
+
     if (!camera.openCamera(0)){
-        std::cout << "Camera is not opened" << std::endl;
+        cout << "Camera is not opened" << endl;
         return -1;
     }
 
-    std::vector<cv::Point3f> vertices = obj.getVertices();
-    std::vector<cv::Point3f> normals = obj.getNormals();
-    std::vector<cv::Point2f> texCoords = obj.getTexCoords();
-    std::vector<Face> faces = obj.getFaces();
-
-    ObjectProjection object(vertices, normals, texCoords, faces);
-
-    camera.showCamera(object);
+    camera.showCamera();
 
     return 0;
 }
